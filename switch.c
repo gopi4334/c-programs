f4e3ce08f7210976fb7ada4fbaee511a904a0295
gopi4334 <61405879+gopi4334@users.noreply.github.com>
2020-03-02 12:23:02 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+ int a,b,c;
+ int ch;
+ printf("enter a and b values");
+ scanf("%d%d",&a,&b);
+ printf("enter choice");
+scanf("%d",&ch);
+switch(ch)
+{
+ case 1:printf("sum=%d",a+b);break;
+ case 2:printf("sum=%d",a-b);break;
+ default:printf("invalid operator");
+}
+}
